83e0423a8d04ae577ffb42b1dd2352f468755b46
Nguyen Dac Liem <nguyendacliem09@gmail.com>
2017-04-27 11:53:06 +0700

    Liem

---
+++ random.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define REPEAT 10
+
+int main( void )
+{
+	int i = 0;
+	srand(time(NULL));
+	printf("MAX: %d\n", RAND_MAX);
+	for ( ; i < REPEAT; ++i){
+
+		printf("%d\n", rand());
+	}
+	printf("\n");
+
+	return 0;
+}
